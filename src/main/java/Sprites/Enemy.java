b9561a2911d682f7346400fdfefdecd0c03d2f22
David <dbermejosimon@gmail.com>
2019-01-23 20:20:45 +0100

    creadas clases Enemy, Item y Skill

--- src/main/java/Sprites/Enemy.java
+++ src/main/java/Sprites/Enemy.java
@@ -1,4 +1,97 @@
 package Sprites;
 
-public class Enemy {
+import javax.swing.*;
+
+/**
+ * <h2>Clase Enemy</h2>
+ * Esta clase gestiona los parametros y valores de los elementos enemigos.
+ * @see Sprite
+ * @author David Bermejo Simon
+ *
+ */
+public class Enemy extends Sprite {
+    boolean isAlive;
+    int totalHp;
+    int atk;
+    int def;
+    JProgressBar life;
+
+
+    /**
+     * Constructor de la clase vacio
+     */
+    public Enemy(){
+
+    }
+
+    /**
+     * Constructor de la clase con todos los parametros tanto de la clase como de la clase Sprite de la que hereda
+     * @param posX
+     * @param posY
+     * @param vX
+     * @param vY
+     * @param id
+     * @param imageRoutes
+     * @param isAlive
+     * @param totalHp
+     * @param atk
+     * @param def
+     * @param life
+     */
+    public Enemy(int posX, int posY, int vX, int vY, String id, String[] imageRoutes, boolean isAlive, int totalHp, int atk, int def, JProgressBar life) {
+        super(posX, posY, vX, vY, id, imageRoutes);
+        this.isAlive = isAlive;
+        this.totalHp = totalHp;
+        this.atk = atk;
+        this.def = def;
+        this.life = life;
+    }
+
+    public Enemy(boolean isAlive, int totalHp, int atk, int def, JProgressBar life) {
+        this.isAlive = isAlive;
+        this.totalHp = totalHp;
+        this.atk = atk;
+        this.def = def;
+        this.life = life;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public int getTotalHp() {
+        return totalHp;
+    }
+
+    public void setTotalHp(int totalHp) {
+        this.totalHp = totalHp;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public JProgressBar getLife() {
+        return life;
+    }
+
+    public void setLife(JProgressBar life) {
+        this.life = life;
+    }
 }
--- src/main/java/Sprites/Item.java
+++ src/main/java/Sprites/Item.java
@@ -1,4 +1,27 @@
 package Sprites;
 
 public class Item {
+
+    String name;
+    String description;
+
+    public Item(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
